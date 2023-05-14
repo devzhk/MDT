8dec3db06fe1df9053446577c6795b67bad2a196
devzhk <devzhk@gmail.com>
2023-05-14 11:51:09 -0700

    update script

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,10 +1,9 @@
 pip install torch torchvision torchaudio
-pip install einops lmdb omegaconf wandb tqdm pyyaml accelerate blobfile mpi4py
+pip install einops lmdb omegaconf wandb tqdm pyyaml accelerate transformers blobfile mpi4py diffusers
 pip install git+https://github.com/huggingface/pytorch-image-models.git
-pip install git+https://github.com/huggingface/diffusers
 
 MODEL_FLAGS="--image_size 256 --model MDT_XL_2 --decode_layer 2"
-DIFFUSION_FLAGS="--num_sampling_steps 250 --num_samples 50000"
+DIFFUSION_FLAGS="--num_sampling_steps 250 --num_samples 50000 --batch_size 50"
 
 NUM_GPUS=8
 
@@ -15,8 +14,8 @@ do
    echo $MODEL_PATH
    export OPENAI_LOGDIR=output_mdt_xl2_eval/$ckpt_id
    echo $OPENAI_LOGDIR
-   python3 -m torch.distributed.launch --nproc_per_node=$NUM_GPUS image_sample.py --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
-   python3 evaluator.py ../assets/fid_stats/VIRTUAL_imagenet256_labeled.npz $OPENAI_LOGDIR/samples_50000x256x256x3.npz
+   torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS image_sample.py --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
+   # python3 evaluator.py ../assets/fid_stats/VIRTUAL_imagenet256_labeled.npz $OPENAI_LOGDIR/samples_50000x256x256x3.npz
 done
 
 
@@ -27,6 +26,6 @@ do
    echo $MODEL_PATH
    export OPENAI_LOGDIR=output_mdt_xl2_eval-256/$ckpt_id
    echo $OPENAI_LOGDIR
-   python3 -m torch.distributed.launch --nproc_per_node=$NUM_GPUS image_sample.py --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
-   python3 evaluator.py ../assets/fid_stats/VIRTUAL_imagenet256_labeled.npz $OPENAI_LOGDIR/samples_50000x256x256x3.npz
+   torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS image_sample.py --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
+   # python3 evaluator.py ../assets/fid_stats/VIRTUAL_imagenet256_labeled.npz $OPENAI_LOGDIR/samples_50000x256x256x3.npz
 done
