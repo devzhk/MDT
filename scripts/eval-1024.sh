79146d08b0c041971d0589a61dadcf371b145f27
devzhk <devzhk@gmail.com>
2023-05-15 22:31:19 -0700

    add eval job

---
+++ scripts/eval-1024.sh
@@ -0,0 +1,23 @@
+pip install torch torchvision torchaudio
+pip install einops lmdb omegaconf wandb tqdm pyyaml accelerate blobfile mpi4py
+pip install git+https://github.com/huggingface/pytorch-image-models.git
+pip install --upgrade transformers
+pip install diffusers["torch"]
+
+MODEL_FLAGS="--image_size 256 --model MDT_XL_2 --decode_layer 2"
+DIFFUSION_FLAGS="--num_sampling_steps 250 --num_samples 50000 --batch_size 50"
+
+NUM_GPUS=8
+
+for ((i=200000; i< 220000; i+=20000))
+do
+   printf -v ckpt_id "%06d" $i
+   MODEL_PATH=output_mdt_xl2-256/ema_0.9999_$ckpt_id.pt
+   echo $MODEL_PATH
+   export OPENAI_LOGDIR=output_mdt_xl2_eval-256/$ckpt_id
+   echo $OPENAI_LOGDIR
+   torchrun --nnodes=1 --nproc_per_node=$NUM_GPUS image_sample.py --model_path $MODEL_PATH $MODEL_FLAGS $DIFFUSION_FLAGS
+   python3 evaluator.py ../assets/fid_stats/VIRTUAL_imagenet256_labeled.npz $OPENAI_LOGDIR/samples_50000x256x256x3.npz
+done
+
+sleep 48h
--- scripts/submit_eval.sh
+++ scripts/submit_eval.sh
@@ -1,7 +1,22 @@
+# ngc batch run \
+# --name "ml-model.MDT-XL2-eval" \
+# --commandline "cd /diffusion_ws/Code/MDT; git config --global --add safe.directory /diffusion_ws/Code/MDT; \
+# bash scripts/eval.sh" \
+# --image "nvidia/tensorflow:22.03-tf2-py3" \
+# --ace nv-us-west-3 \
+# --instance dgxa100.80g.8.norm \
+# --priority NORMAL \
+# --total-runtime 168h \
+# --workspace ReY8v22xQoqdGPDyMwZ75g:/diffusion_ws \
+# --datasetid 1602645:/imagenet_256_latent_lmdb \
+# --result /results \
+# --label ml__vitdiff_hk --label _wl__computervision \
+# --port 6066 --port 1234 --port 8888
+
 ngc batch run \
---name "ml-model.MDT-XL2-eval" \
+--name "ml-model.MDT-XL2-eval-1024" \
 --commandline "cd /diffusion_ws/Code/MDT; git config --global --add safe.directory /diffusion_ws/Code/MDT; \
-bash scripts/eval.sh" \
+bash scripts/eval-1024.sh" \
 --image "nvidia/tensorflow:22.03-tf2-py3" \
 --ace nv-us-west-3 \
 --instance dgxa100.80g.8.norm \
